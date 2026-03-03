8a698d971611b4f27a171c36849d321a2890d20e
mw <jackwoong322@gmail.com>
2026-03-03 21:04:37 +0900

    불필요한 파일 삭제

--- pairUSDT/extract_script.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('pairUSDT/033_visualizer_html.html', 'utf8');
-const start = html.indexOf('<script>');
-const end = html.indexOf('</script>', start);
-const script = html.slice(start + 8, end);
-fs.writeFileSync('pairUSDT/extracted_script.js', script, 'utf8');
-console.log('Extracted', script.length, 'chars');
